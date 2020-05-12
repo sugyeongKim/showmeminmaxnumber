21c754dc4e860443761cf3aa630768329bed02df
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-05-12 13:49:11 +0900

    7번 문제

--- showmeminmaxnumber/Program.cs
+++ showmeminmaxnumber/Program.cs
@@ -10,6 +10,29 @@ namespace showmeminmaxnumber
     {
         static void Main(string[] args)
         {
+            int[] number = new int[5];
+
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.Write("숫자를 입력해주세요: ");
+                number[i] = int.Parse(Console.ReadLine());
+            }
+
+            int max = 0, min = number[0];
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (max < number[i])
+                    max = number[i];
+                if (min > number[i])
+                    min = number[i];
+            }
+
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("가장 큰 수: " + max);
+            Console.WriteLine("가장 작은 수: " + min);
         }
     }
 }
